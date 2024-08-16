b582da891efa76491ae55c2262df4222c6dd4d78
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-08-16 19:02:29 +0700

    Commit docker init

--- InSyncAPI/InSyncAPI/Program.cs
+++ InSyncAPI/InSyncAPI/Program.cs
@@ -12,7 +12,7 @@ namespace InSyncAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,7 +23,7 @@ namespace InSyncAPI
             }
 
             app.UseAuthorization();
-
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             var summaries = new[]
             {
                 "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -31,17 +31,19 @@ namespace InSyncAPI
 
             app.MapGet("/weatherforecast", (HttpContext httpContext) =>
             {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateTime.Now.AddDays(index),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
+                var forecast = new[]
+                {
+        new { id = 0, name = "Templates & Plan Features", price = "Price", projects = "Projects", scenarios = "Number of Scenarios", support = "Support", storage = "Storage", users = "Number of Users", apiAccess = "API Access" },
+        new { id = 1, name = "Starters", price = "Free", projects = "3", scenarios = "3", support = "Weekdays", storage = "5GB", users = "1", apiAccess = "No" },
+        new { id = 2, name = "Professional", price = "100$/Month", projects = "Unlimited", scenarios = "Unlimited", support = "24/7", storage = "Unlimited", users = "10", apiAccess = "Yes" }
+    };
+
+                httpContext.Response.Headers["Content-Type"] = "application/json";
+                return Results.Json(forecast);
             })
-            .WithName("GetWeatherForecast");
+.WithName("GetWeatherForecast");
+
+
 
             app.Run();
         }
