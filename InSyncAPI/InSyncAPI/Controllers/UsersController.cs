f12248ac86a762eaceb48cf8574e84cacb1f928d
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-09-04 16:58:16 +0700

    commit clerk update

--- InSyncAPI/InSyncAPI/Controllers/UsersController.cs
+++ InSyncAPI/InSyncAPI/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using BusinessObjects.Models;
+using InSyncAPI.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Repositorys;
 
 namespace InSyncAPI.Controllers
 {
@@ -7,5 +11,31 @@ namespace InSyncAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private IUserRepository _userRepo;
+        private IMapper _mapper;
+        //Retrive payload from clerk
+
+        public UsersController(IUserRepository userRepo, IMapper mapper)
+        {
+            _userRepo = userRepo;
+            _mapper = mapper;
+        }
+        [HttpPost("clerk-web-hook")]
+        public async Task<IActionResult> Post([FromBody] CreateUserDto userDto)
+        {
+            if (_userRepo == null || _mapper == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                       value: "Application service has not been created");
+            }
+            var user = _mapper.Map<User>(userDto);
+            var response = await _userRepo.Add(user);
+            if (response == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    value: "Error occurred while adding the project.");
+            }
+            return Ok(user);
+        }
     }
 }
