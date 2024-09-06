b6d8f540528601f8b3f7c0002c0f8ef223a16995
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-09-06 15:50:47 +0700

    commit

--- InSyncAPI/InSyncAPI/Dtos/UserDto.cs
+++ InSyncAPI/InSyncAPI/Dtos/UserDto.cs
@@ -6,7 +6,7 @@ namespace InSyncAPI.Dtos
     public class Data
     {
         public string? Birthday { get; set; }
-        public long? Created_At { get; set; }
+        public long Created_At { get; set; }
         public EmailAddresses[]? Email_Addresses { get; set; }
         public string[]? External_Accounts { get; set; }
         public string? First_Name { get; set; } = "";
@@ -17,7 +17,7 @@ namespace InSyncAPI.Dtos
         public long? Last_Sign_In_At { get; set; }
         public string? Object { get; set; }
         public bool? Password_Enabled { get; set; }
-        public string[]? Phone_Numbers { get; set; };
+        public string[]? Phone_Numbers { get; set; }
         public string? Primary_Email_Address_Id { get; set; }
         public string? Primary_Phone_Number_Id { get; set; }
         public string? Primary_Web3_Wallet_Id { get; set; }
@@ -26,7 +26,7 @@ namespace InSyncAPI.Dtos
         public Dictionary<string, object>? Public_Metadata { get; set; }
         public bool? Two_Factor_Enabled { get; set; }
         public Dictionary<string, object>? Unsafe_Metadata { get; set; }
-        public long? Updated_At { get; set; }
+        public long Updated_At { get; set; }
         public object? Username { get; set; }
         public string[]? Web3_Wallets { get; set; }
     }
