41a9e2b05f79efebb8ce54467211049a646d8d74
Buiminh731 <minhbqhe176321@fpt.edu.vn>
2024-09-06 15:49:12 +0700

    commit

--- InSyncAPI/InSyncAPI/Dtos/UserDto.cs
+++ InSyncAPI/InSyncAPI/Dtos/UserDto.cs
@@ -5,30 +5,30 @@ namespace InSyncAPI.Dtos
 {
     public class Data
     {
-        public string Birthday { get; set; }
-        public long Created_At { get; set; }
-        public EmailAddresses[] Email_Addresses { get; set; }
-        public string[] External_Accounts { get; set; }
-        public string First_Name { get; set; }
-        public string Gender { get; set; }
-        public string Id { get; set; }
-        public string Image_Url { get; set; }
-        public string Last_Name { get; set; }
-        public long Last_Sign_In_At { get; set; }
-        public string Object { get; set; }
-        public bool Password_Enabled { get; set; }
-        public string[] Phone_Numbers { get; set; }
-        public string Primary_Email_Address_Id { get; set; }
-        public string Primary_Phone_Number_Id { get; set; }
-        public string Primary_Web3_Wallet_Id { get; set; }
-        public Dictionary<string, object> Private_Metadata { get; set; }
-        public string Profile_Image_Url { get; set; }
-        public Dictionary<string, object> Public_Metadata { get; set; }
-        public bool Two_Factor_Enabled { get; set; }
-        public Dictionary<string, object> Unsafe_Metadata { get; set; }
-        public long Updated_At { get; set; }
-        public object Username { get; set; }
-        public string[] Web3_Wallets { get; set; }
+        public string? Birthday { get; set; }
+        public long? Created_At { get; set; }
+        public EmailAddresses[]? Email_Addresses { get; set; }
+        public string[]? External_Accounts { get; set; }
+        public string? First_Name { get; set; } = "";
+        public string? Gender { get; set; }
+        public string? Id { get; set; }
+        public string? Image_Url { get; set; }
+        public string? Last_Name { get; set; } = "";
+        public long? Last_Sign_In_At { get; set; }
+        public string? Object { get; set; }
+        public bool? Password_Enabled { get; set; }
+        public string[]? Phone_Numbers { get; set; };
+        public string? Primary_Email_Address_Id { get; set; }
+        public string? Primary_Phone_Number_Id { get; set; }
+        public string? Primary_Web3_Wallet_Id { get; set; }
+        public Dictionary<string, object>? Private_Metadata { get; set; }
+        public string? Profile_Image_Url { get; set; } = "";
+        public Dictionary<string, object>? Public_Metadata { get; set; }
+        public bool? Two_Factor_Enabled { get; set; }
+        public Dictionary<string, object>? Unsafe_Metadata { get; set; }
+        public long? Updated_At { get; set; }
+        public object? Username { get; set; }
+        public string[]? Web3_Wallets { get; set; }
     }
     public class EmailAddresses
     {
