9f6fad22196f31337a5b2a38e113579d903f6ac4
HuyKhongBietCode <phanhuy05092002@gmail.com>
2024-11-19 14:59:51 +0700

    update code

--- InSyncAPI/InSyncAPI/Program.cs
+++ InSyncAPI/InSyncAPI/Program.cs
@@ -69,7 +69,7 @@ namespace InSyncAPI
                 builder
                 .AllowAnyHeader()
                 .AllowAnyMethod()
-               .WithOrigins(urlOrigins)
+              .AllowAnyOrigin()
                 .SetIsOriginAllowed((host) => true));
 
             });
